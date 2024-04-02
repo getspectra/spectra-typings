7380c940e4105fa4406bbf417515bfc8cc0a697f
Palm Civet <palmcivet_email@qq.com>
2024-04-02 19:43:17 +0800

    feat: setup @getspectra/spectra-typings

---
+++ README.md
@@ -0,0 +1,5 @@
+# @getspectra/spectra-typings
+
+[![NPM Version](https://img.shields.io/npm/v/%40getspectra%2Fspectra-typings)](https://www.npmjs.com/package/@getspectra/spectra-typings) [![License](https://img.shields.io/github/license/getspectra/spectra-typings)](./LICENSE)
+
+Type definitions for [@getspectra/spectra-js](https://github.com/getspectra/spectra-js) and [@getspectra/spectra-components](https://github.com/getspectra/spectra-components).
---
+++ index.d.ts
@@ -0,0 +1,33 @@
+export type NumberOperation = '=' | '!=' | '<>' | '>' | '>=' | '<' | '<=';
+
+export type ArrayOperation = 'in' | 'nin' | 'not_in';
+
+export type Operation = NumberOperation | ArrayOperation;
+
+export type FieldName = `${string}.${string}`;
+
+export type FieldValue = ArgumentValue | ArgumentRef;
+
+export type ArgumentValue = string | boolean | number | Date | null;
+
+export type ArgumentRef = { type: 'field'; ref: FieldName };
+
+export type ExpressionDefine =
+  | BinaryExpressionDefine
+  | OrExpressionDefine
+  | AndExpressionDefine
+  | NotExpressionDefine;
+
+export type BinaryExpressionDefine = [FieldName, Operation, FieldValue];
+
+export type OrExpressionDefine = {
+  or: Array<ExpressionDefine>;
+};
+
+export type AndExpressionDefine = {
+  and: Array<ExpressionDefine>;
+};
+
+export type NotExpressionDefine = {
+  not: ExpressionDefine;
+};
